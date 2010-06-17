c7977ffa1d1366399846182f3343242778673698
Matz Radloff <matzradloff@gmx.de>
2010-06-17 10:20:59 +0200

    Initial commit

---
+++ test.py
@@ -0,0 +1,67 @@
+#! /usr/bin/env python
+
+import pygame
+import sys
+import random
+
+class Ball(object):
+	def __init__(self, resource, pos):
+		self.Image = pygame.image.load(resource)
+		self.BallRect = self.Image.get_rect()
+		self.Speed = [random.randint(1,2), random.randint(1,2)]
+		self.BallRect.move_ip(pos[0], pos[1])
+
+	def draw(self, screen):
+		screen.blit(self.Image, self.BallRect)
+
+	def move(self):
+		self.BallRect.move_ip(self.Speed)
+
+		if self.BallRect.left < 0 or self.BallRect.right > 1280:
+			self.Speed[0] = -self.Speed[0]
+		if self.BallRect.top < 0 or self.BallRect.bottom > 800:
+			self.Speed[1] = -self.Speed[1]
+
+		for ball in balls:
+			if (ball != self) or self.BallRect.colliderect(ball.BallRect):
+
+				if self.Speed[0] > 0:
+					self.BallRect.right = ball.BallRect.left
+
+				elif self.Speed[0] < 0:
+					self.BallRect.left = ball.BallRect.right
+
+				if self.Speed[1] > 0:
+					self.BallRect.bottom = ball.BallRect.top
+
+				elif self.Speed[1] < 0:
+					self.BallRect.top = ball.BallRect.bottom
+
+
+
+pygame.init();
+
+size = width, height = 1280, 800
+
+speed = [2, 2]
+
+screen = pygame.display.set_mode(size)
+
+balls = []
+
+for x in xrange(2):
+	balls.append(Ball("ball.png", [x*200, x*100]))
+
+while True:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT: sys.exit()
+
+	for ball in balls:
+		ball.move()
+
+	screen.fill((0,0,0))
+
+	for ball in balls:
+		ball.draw(screen)
+
+	pygame.display.flip()
